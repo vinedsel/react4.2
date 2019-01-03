d99e84d1e95b33e3cc1534edc512a9f261b6092f
Raymond Gregory <vinedsel@gmail.com>
2019-01-02 19:59:14 -0600

    Code mostly working.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
-alert("it's alive");
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = props => {
+  return <NumberOfStudents />;
+};
+
+class NumberOfStudents extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      students: 87,
+      addStudents: 0
+    };
+  }
+
+  incrementOne = () => {
+    this.setState({
+      students: this.state.students + 1
+    });
+  }
+
+
+  increment = () => {
+    this.setState({
+      students: this.state.students + parseInt(this.state.addStudents)
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Enrolled Students: {this.state.students}</h1>
+        <button onClick={this.increment.bind(this)}>Add 1 Enrolled Student</button>
+        <br />
+        <h3>Add Multiple Enrolled Students</h3>
+        <input
+          type="number"
+          onChange={event => this.setState({ addStudents: event.target.value })}
+          value={this.state.addStudents}
+        />
+        <button onClick={this.increment.bind(this)}>Increase Students</button>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
